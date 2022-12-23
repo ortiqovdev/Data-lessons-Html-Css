7603ae66ff659abf16da07d8a95dd016b7811e39
Akmal Oriqov <akmalbekortiqov7976@gmail.com>
2022-12-24 00:22:07 +0500

    Data ls lessons

---
+++ 1.--Data-- Html and css/2ish/index.cs
@@ -0,0 +1,12 @@
+using System;
+public class Hello World
+{
+    public static viod Main(string[] args)
+    {
+        int[] natural_son = {1,32,35,37,76,98,5,6,8,9};
+        for (int i=0, i<natural_son.Length;i++)
+        {
+            consol.WriteLine(natural_son[i])
+        }
+    }
+}
